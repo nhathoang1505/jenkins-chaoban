92b626541d29194ecfc6e02f49612cca292d319a
nhathoang1505 <nhathoang1505dmx@gmail.com>
2025-09-09 22:12:39 +0700

    Create WebServer.java

---
+++ WebServer.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+
+public class WebServer {
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8081), 0);
+        server.createContext("/", new HelloHandler());
+        server.setExecutor(null);
+        server.start();
+        System.out.println("Server started at http://localhost:8081");
+    }
+    static class HelloHandler implements HttpHandler {
+        public void handle(HttpExchange exchange) throws IOException {
+            String response = "Hello World from Jenkins + GitHub!";
+            exchange.sendResponseHeaders(200, response.length());
+            try (OutputStream os = exchange.getResponseBody()) {
+                os.write(response.getBytes());
+            }
+        }
+    }
+}
